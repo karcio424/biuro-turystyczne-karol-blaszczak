eb5066d24eb9747822df6c393ea07bcd9fc632c7
Karol Błaszczak <92331517+karcio424@users.noreply.github.com>
2024-02-15 05:20:23 +0100

    update

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+      apiKey: "AIzaSyDUazZPKSULWNXBb-qMxImMUhX594P7C2Q",
+      authDomain: "tanie-zwiedzanie-d43e9.firebaseapp.com",
+      databaseURL: "https://tanie-zwiedzanie-d43e9-default-rtdb.firebaseio.com/",
+      projectId: "tanie-zwiedzanie-d43e9",
+      storageBucket: "tanie-zwiedzanie-d43e9.appspot.com",
+      messagingSenderId: "807669871338",
+      appId: "1:807669871338:web:d5f206f408e5bbce175d93",
+    },
+  };
